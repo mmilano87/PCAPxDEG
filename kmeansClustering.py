8f3f8ebb8a8fc8e2a83b29c7143ae02fe04e9714
mmilano87 <51692146+mmilano87@users.noreply.github.com>
2022-10-03 17:02:19 +0200

    Add files via upload

---
+++ kmeansClustering.py
@@ -0,0 +1,50 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MaxAbsScaler
+
+#the variable containing the path to the input gene expression data set to analyse
+data = "./data.csv"
+
+# Gene expression data set is red using NumPy genfromtxt, setting "," as delimiter
+# and skipping first column and row since containing the headers info
+gene_exp_val = np.genfromtxt(data, delimiter=",", usecols=range(1, 1001), skip_header=1).transpose()
+
+#definition of the MaxAbsScaler estimator object
+scaler = MaxAbsScaler()
+#run the estimator and store the result into the variable transformed_data
+transformed_data = scaler.fit_transform(gene_exp_val)
+
+#definition and execution of the pca object 
+pca_data = PCA(n_components=2, random_state=78).fit_transform(transformed_data)
+
+#definition and execution of the kmeans object 
+kmeans=KMeans(n_clusters=nu_clusters, init="k-means++", n_init=50, max_iter=500, random_state=42).fit(pca)
+
+data_frame = pd.DataFrame(pca)
+cluster_map = pd.DataFrame()
+cluster_map['data_index'] = data_frame.index.values
+cluster_map['cluster'] = kmeans.labels_
+
+plt.style.use("fivethirtyeight")
+plt.figure(figsize=(8, 6)) #
+sns.scatterplot(
+"X", "Y",
+s=50,
+data=dataframe,
+hue="clusters",
+palette="Set2")
+lg = plt.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
+
+plt.savefig(fname='./clusters.eps',
+            dpi=300,
+            format='eps',
+            bbox_extra_artists=(lg,),
+            bbox_inches='tight')
+plt.tight_layout()
+plt.show()
